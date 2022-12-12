b79abd8678e04908901dfc2236829830dde16f41
renjisen <rjs@123.com>
2022-12-12 20:48:03 +0800

    gitee first commit

--- src/main/java/com/rjs/control/partControl/CheckDataControl.java
+++ src/main/java/com/rjs/control/partControl/CheckDataControl.java
@@ -32,7 +32,5 @@ public class CheckDataControl {
         System.out.println(22222);
         System.out.println(33333);
         System.out.println(44444);
-        System.out.println(55555);
-        System.out.println(66666);
     }
 }
